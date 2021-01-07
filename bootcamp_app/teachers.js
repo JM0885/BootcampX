b0d6da8aced2ea60a2abbe90bd271225c9c13fa1
Jared Mosher <jared.mosher@gmail.com>
2021-01-07 23:06:55 +0000

    Add paramaterized queries to teachers.js and students.js

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,7 +7,8 @@ const pool = new Pool({
   database: 'vagrant'
 });
 
-pool.query(`
+
+const queryString = `
 SELECT DISTINCT
   teachers.name as teacher,
   cohorts.name as cohort,
@@ -16,10 +17,15 @@ FROM teachers
 JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
+WHERE cohorts.name LIKE $1 || 'JUL02'
 GROUP BY teacher, cohort
 ORDER BY teacher;
-`)
+`;
+
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
